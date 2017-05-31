600cfbc15ee9496d63273b54c6dca5ffe79786ee
Matt Oskamp <mattoskamp@gmail.com>
2017-05-30 20:53:40 -0400

    teammates are viewable through the stats page

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	
+	$.get('/api/team').done(function(teams) {
+		teams.forEach(function(team) {
+			$('#row-'+team._id).click(function(e) {
+				showTeamInfo(team);
+			});
+		});
+	});
 });
+
+function showTeamInfo(team) {
+	var playerList = $('#playerList')
+	playerList.empty();
+	team.teammates.forEach(function(player) {
+		playerList.append($('<li class="list-group-item"></li>').text(player.name));
+	});
+}
